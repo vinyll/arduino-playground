9e5e6d8b1167e2fb8ae55f65aa4b754b8a02f97b
Vincent Agnano <vincent.agnano@scopyleft.fr>
2012-12-04 19:41:23 +0100

    fixed python led-pulse

--- python/led-pulse.py
+++ python/led-pulse.py
@@ -1,22 +1,8 @@
 import time
 from pyfirmata import Arduino, util
 
-board=Arduino('/dev/tty.usbmodem641')
-
+board = Arduino('/dev/tty.usbmodem411')
 led = board.get_pin('d:13:o')
-
-def go_green():
-    y.write(1)
-    time.sleep(DELAY)
-    r.write(0)
-    g.write(1)
-    y.write(0)
-    time.sleep(GREEN)
-    y.write(1)
-    time.sleep(DELAY)
-    g.write(0)
-    y.write(0)
-
 
 while True:
     led.write(1)
@@ -27,5 +13,3 @@ while True:
     time.sleep(1)
     led.write(0)
     time.sleep(1)
-
-board.exit()
