e4f32922e1d07176928fc0713bc5a34e715fac10
Vincent Agnano <vincent.agnano@scopyleft.fr>
2012-12-09 10:12:42 +0100

    updated temperature script

--- nodejs/temperature.js
+++ nodejs/temperature.js
@@ -5,19 +5,16 @@ board = new five.Board();
 
 board.on("ready", function() {
   sensor = new five.Sensor({ pin: 8, freq: 250 });
-  board.repl.inject({
-    sensor: sensor
-  });
 
   sensor.on("change", function(err, reading) {
     var voltage = reading * .004882814;
 
-    // For Fahrenheit
-    // var temperature = (((voltage - .5) * 100)*1.8) + 32;
-
     // For Celsius
     var temperature = ((voltage - .5) * 100);
 
-    console.log( arguments );
+    // For Fahrenheit
+    // var temperature = (((voltage - .5) * 100)*1.8) + 32;
+
+    console.log( temperature );
   });
 });
